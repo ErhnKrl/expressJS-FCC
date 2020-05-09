e30c378ebbc89d858daf9142d1d6db9f2b828444
Ozar <ye.koral@gmail.com>
2020-05-09 06:39:17 +0300

    instead of postgresql mongo db is defined and post method handled

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,14 +1,5 @@
 var mongoose = require('mongoose');
 
-exports.get_landing = function (req, res, next) {
-  res.render('landing', { title: 'Express' });
-};
-
-exports.submit_lead = function (req, res, next) {
-  console.log('lead_email: ', req.body.lead_email); //we gonna access the value in the input element
-  res.redirect('/');
-};
-
 //connect to the database
 mongoose.connect(
   'mongodb+srv://erhan:21210Mak*@cluster0-wycgf.mongodb.net/FCCNodeJS?retryWrites=true&w=majority'
@@ -22,7 +13,15 @@ var emailSchema = new mongoose.Schema({
 //create a model
 var email = mongoose.model('emails', emailSchema);
 
-var itemOne = email({ email: 'deneme3@gmail.com' }).save(function (err) {
-  if (err) throw err;
-  console.log('item saved');
-});
+exports.get_landing = function (req, res, next) {
+  res.render('landing', { title: 'Express' });
+};
+
+//get data from the view and send it to the database
+exports.submit_lead = function (req, res, next) {
+  let newRecord = email({ email: req.body.lead_email }).save(function (err) {
+    if (err) throw err;
+  });
+  console.log('lead_email: ', req.body.lead_email); //we gonna access the value in the input element
+  res.redirect('/');
+};
